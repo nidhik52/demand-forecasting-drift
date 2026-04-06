abd82ba73502f88b04ee4a431c026eb7116b1745
nidhik52 <168454674+nidhik52@users.noreply.github.com>
2026-04-06 12:07:02 +0000

    Some github actions and render files were updated to support optional installation of Prophet and its dependencies. The API was also modified to run the pipeline asynchronously, allowing for faster responses when starting the pipeline.

--- setup.sh
+++ setup.sh
@@ -19,13 +19,18 @@ pip install --upgrade pip setuptools wheel
 pip install setuptools==68.2.2
 pip install -r requirements.txt
 
-# Install CmdStan
-echo "Installing CmdStan..."
-python -c "
+if [ "${ENABLE_PROPHET}" = "1" ]; then
+    echo "Installing Prophet + CmdStan dependencies..."
+    pip install -r requirements-prophet.txt
+
+    # Install CmdStan
+    echo "Installing CmdStan..."
+    python -c "
 import cmdstanpy
 import os
 os.environ['CXXFLAGS'] = '-O0 -g0 -std=c++14'
 cmdstanpy.install_cmdstan(version='2.33.1', dir=os.path.expanduser('~/.cmdstan'), overwrite=True)
 "
+fi
 
 echo "Setup complete! Activate with: source .venv/bin/activate"
